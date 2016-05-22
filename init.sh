ffbcd08e3da7c1a80651966a6e1e97e35d6d90a8
amolsh <amolsh@cybage.com>
2016-05-22 09:36:33 +0530

    added appCheck

--- init.sh
+++ init.sh
@@ -5,9 +5,16 @@ echo  "\tRunning Ansible Script";
 echo "#######################################################";
 echo  "\n";
       ansible-playbook -i hosts site.yml
+
+
+echo "#######################################################";
+echo  "\tChecking website up or not";
+echo "#######################################################";
+
       ansible-playbook -i hosts appCheck.yml
+
 echo  "\n";
 echo "#######################################################";
-echo  "\tRancher installation finished";
+echo  "\tDeployment finished";
 echo "#######################################################";
 
